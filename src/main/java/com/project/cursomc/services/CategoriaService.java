45c2ce1603406365d74ee356b8c161a3fba677ac
Mario23junior <mariojunior3251@gmail.com>
2020-07-09 05:28:48 -0300

    Criando repository e camada de service para categoria

---
+++ src/main/java/com/project/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.project.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.cursomc.domain.Categoria;
+import com.project.cursomc.repositores.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+    
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+			Optional<Categoria> obj = repo.findById(id);
+			return obj.orElse(null);
+	}
+}
